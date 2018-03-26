7bde7243c9002c13f25c018a3ce0b614ba8b0c1a
Kedanco <kelvinchowjunjie120@hotmail.com>
2018-03-26 21:02:04 +0800

    Shifted reduxstarter repo files into folder, created components, init index.js, installed youtube api search npm package

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+
+// import App from './components/app';
+// import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
+
+const API_KEY = 'AIzaSyCA8w4dPUX7BmOj4q3qq5uAzG1Hx-G_f0E';
+
+// Create a new component. This component should produce some HTML
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
