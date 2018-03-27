08f9f21aefd546a2b7f3f1a9eb2f7caaddbb99b6
Kedanco <kelvinchowjunjie120@hotmail.com>
2018-03-27 11:51:57 +0800

    section 2, learned about props, iframe

--- src/index.js
+++ src/index.js
@@ -15,20 +15,36 @@
 //   , document.querySelector('.container'));
 
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video-list';
 
 const API_KEY = 'AIzaSyCA8w4dPUX7BmOj4q3qq5uAzG1Hx-G_f0E';
 
 // Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render(){
+        // passing prop videos to VideoList
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
